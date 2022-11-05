58ecc7cd8d429ab3d4a9e7e41313c890482ede70
Kancoo <80716832+Kancoo@users.noreply.github.com>
2022-11-05 18:54:35 +0100

    Lavarel

---
+++ Studenske_turnajeDB.sql
@@ -0,0 +1,35 @@
+
+drop table user;
+drop table tournament;
+drop table match;
+drop table team;
+
+CREATE TABLE user (
+    column1 datatype,
+    column2 datatype,
+    column3 datatype,
+   ....
+);
+
+CREATE TABLE tournament (
+    column1 datatype,
+    column2 datatype,
+    column3 datatype,
+   ....
+);
+
+CREATE TABLE match (
+    column1 datatype,
+    column2 datatype,
+    column3 datatype,
+   ....
+);
+
+CREATE TABLE team (
+    column1 datatype,
+    column2 datatype,
+    column3 datatype,
+   ....
+);
+
+
