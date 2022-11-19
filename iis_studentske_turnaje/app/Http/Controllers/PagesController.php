b561559f1a9c8a64ae1f4b4be36a0ec9c97efa9d
MatejHorn <67945994+MaHorna@users.noreply.github.com>
2022-11-19 11:58:05 +0100

    routing and views created

---
+++ iis_studentske_turnaje/app/Http/Controllers/PagesController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    public function login() {
+	return view('pages.login');
+    }
+    public function admin() {
+	return view('pages.admin');
+    }
+    public function loged_user() {
+	return view('pages.loged_user');
+    }
+    public function nonuser() {
+	return view('pages.nonuser');
+    }
+    public function profile() {
+	return view('pages.profile');
+    }
+    public function team_create() {
+	return view('pages.team_create');
+    }
+    public function tour_create() {
+	return view('pages.tour_create');
+    }
+    public function my_tour() {
+	return view('pages.my_tour');
+    }
+    public function tournaments() {
+	return view('pages.tournaments');
+    }
+    public function users() {
+	return view('pages.users');
+    }
+    public function teams() {
+	return view('pages.teams');
+    }
+    public function tournament($id) {
+	return view('pages.tournament');
+    }
+    public function user($id) {
+	return view('pages.user');
+    }
+    public function team($id) {
+	return view('pages.team');
+    }
+}
