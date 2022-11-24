894a9735b72145ce48d7be6ab6181bd6e5413b0a
Kancoo <80716832+Kancoo@users.noreply.github.com>
2022-11-24 16:35:16 +0100

    fix

--- iis_studentske_turnaje/resources/views/tournaments/show.blade.php
+++ iis_studentske_turnaje/resources/views/tournaments/show.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    @include('partials._search')
+    <x-search :path="'/tournaments'"/>
         <a href="{{url('/')}}" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back</a>
         <div class="mx-4">
             <x-card class="p-10">
