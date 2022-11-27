4b20ccecbff3323896ec151ae3be0688b3a43236
MatejHorn <67945994+MaHorna@users.noreply.github.com>
2022-11-27 16:12:50 +0100

    visual fixes

--- iis_studentske_turnaje/resources/views/components/match-card.blade.php
+++ iis_studentske_turnaje/resources/views/components/match-card.blade.php
@@ -1,10 +1,8 @@
 @props(['contest', 'tournament'])
 
-<x-card>
+<div>
     @if ($tournament->teams_allowed == true)
         @php
-            
-        
             $team1 = App\Models\Team::join('contestants', 'contestants.team_id', '=', 'teams.id')
                 ->where('tournament_id', $tournament->id)
                 ->join('contests', 'contestants.id', '=', 'contests.contestant1_id')
@@ -16,12 +14,21 @@
                 ->where('contestants.id', $contest->contestant2_id)
                 ->first();
         @endphp
+
         <div style="margin: 15px 0; overflow: hidden; border-radius: 5px;">
             <div style="color: #fff; padding: 10px 8px; background-color: #74b9ff;border-bottom: 1px solid #8fc7ff;">
-                <a href="{{url('/teams', $team1->id)}}">{{$team1->name}}</a><p style="text-align: right;">{{$contest->score1}}</p>
+                @if (!is_null($user1))
+                    <a href="{{url('/teams', $team1->id)}}">{{$team1->name}}</a><span style="text-align: right;">{{$contest->score1}}</span>
+                @else
+                    <span>-</span>
+                @endif
             </div>
-            <div style="color: #fff; padding: 10px 8px; background-color: #74b9ff;border-bottom: 1px solid #8fc7ff;">
-                <a href="{{url('/teams', $team2->id)}}">{{$team2->name}}</a><p style="text-align: right;">{{$contest->score2}}</p>
+            <div style="color: #fff; padding: 10px 8px; background-color: #74b9ff;">
+                @if (!is_null($user2))
+                    <a href="{{url('/teams', $team2->id)}}">{{$team2->name}}</a><span style="text-align: right;">{{$contest->score2}}</span>
+                @else
+                    <span>-</span>
+                @endif
             </div>
         </div>
     @else
@@ -39,21 +46,20 @@
         @endphp
         
         <div style="margin: 15px 0; overflow: hidden; border-radius: 5px;">
-            
-                <div style="color: #fff; padding: 10px 8px; background-color: #74b9ff;border-bottom: 1px solid #8fc7ff;">
-                    @if (!is_null($user1))
-                    <a href="{{url('/users', $user1->id)}}">{{$user1->name}}</a><p style="text-align: right;">{{$contest->score1}}</p>
-                    @endif
-                </div>
-            
-            
-            
-                <div style="color: #fff; padding: 10px 8px; background-color: #74b9ff;border-bottom: 1px solid #8fc7ff;">
-                    @if (!is_null($user2))
-                    <a href="{{url('/users', $user2->id)}}">{{$user2->name}}</a><p style="text-align: right;">{{$contest->score2}}</p>
-                    @endif
-                </div>
-            
+            <div style="color: #fff; padding: 10px 8px; background-color: #74b9ff;border-bottom: 1px solid #8fc7ff;">
+                @if (!is_null($user1))
+                    <a href="{{url('/users', $user1->id)}}">{{$user1->name}}</a><span style="text-align: right;">{{$contest->score1}}</span>
+                @else
+                    <span>-</span>
+                @endif
+            </div>
+            <div style="color: #fff; padding: 10px 8px; background-color: #74b9ff;">
+                @if (!is_null($user2))
+                    <a href="{{url('/users', $user2->id)}}">{{$user2->name}}</a><span style="text-align: right;">{{$contest->score2}}</span>
+                @else
+                    <span>-</span>
+                @endif
+            </div>
         </div>
     @endif
-</x-card>
+</div>
